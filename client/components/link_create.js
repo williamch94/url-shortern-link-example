933dce182cdee732d1eec7378edacc70fbaaea77
William Chow <williamypchow@gmail.com>
2016-11-16 11:46:19 +1100

    first commit

---
+++ client/components/link_create.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class LinkCreate extends Component {
+  handleSubmit(event){
+    event.preventDefault();
+
+    
+    Meteor.call('links.insert', this.refs.link.value);
+  }
+
+  render (){
+    return(
+      <form onSubmit={this.handleSubmit.bind(this)}>
+      <div className="form-group">
+        <label>Link to shorten</label>
+        <input ref="link" className="form-control"/>
+      </div>
+      <button className="btn btn-primary">Shorten!</button>
+      </form>
+    );
+  }
+}
+
+export default LinkCreate;
